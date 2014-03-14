dd7b494f2e6d97f64c730224c4f9efd65317b28d
Samuel Kim <samuelkim@college.harvard.edu>
2014-03-14 18:19:46 -0400

    README file for the warmup, edited warmup section of write-up

---
+++ Warmup/main_logRegress.py
@@ -0,0 +1,19 @@
+from classification import *
+from utils import *
+import scipy.linalg
+
+K = 3
+xs, fruit = importFruitData()
+
+w1 = logisticRegression(xs, fruit, K)
+plotRegression(xs, fruit, w1, y, K)
+
+#w2 = generativeClassifierWithBayesian(xs, fruit, K)
+#lotRegression(xs, fruit, w2, y, K)
+
+# w = logRegress(basisNone(xs), vectT(fruit, K))
+# print w
+# plotRegression(xs, fruit, w, y, K)
+
+
+plt.show()
