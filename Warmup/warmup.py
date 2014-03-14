ee6456523496a1f689ec8a03ac01c851d12b5bd2
rhedshi <rhedshi@Rheds-MacBook-Pro.local>
2014-03-14 16:26:09 -0400

    blah

--- Warmup/warmup.py
+++ Warmup/warmup.py
@@ -4,11 +4,12 @@ import scipy.linalg
 
 K = 3
 xs, fruit = importFruitData()
-N, mu = mean(xs, fruit, K)
-print mu
-print type(class_split(xs)[0])
 
-w = logisticRegression(xs, fruit, K)
-plotRegression(xs,fruit, w, K)
+# w1 = logisticRegression(xs, fruit, K)
+# plotRegression(xs, fruit, w1, y, K)
+
+w2 = generativeClassifierWithBayesian(xs, fruit, K)
+plotRegression(xs, fruit, w2, y, K)
+
 
 plt.show()
